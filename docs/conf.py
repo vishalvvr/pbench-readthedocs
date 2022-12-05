13f34bc1702ab11421e1bbf156e25de8978573c8
Nick Dokos <ndokos@redhat.com>
2022-12-05 15:29:56 -0500

    Add myst_parser to requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import os
+import sys
 
 # -- Project information -----------------------------------------------------
 
@@ -42,7 +43,6 @@ release = ""
 extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx_design",
-    "myst_paser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -62,7 +62,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
